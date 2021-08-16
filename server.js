306b6caef9f0cb800c5ba1c18a33dab9d36f124c
Akhilsv <akhilsvbhat@gmail.com>
2021-08-16 23:51:14 +0530

    npm i

--- server.js
+++ server.js
@@ -1,25 +1,19 @@
-import express from 'express';
-import  Db  from './config/Db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import router from './Routes/routes.js';
-
+import express from 'express';
+import Db from './Config/Db.js';
+import router from './Routers/index.js';
 dotenv.config();
 Db();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
+app.get('/', (req, res) => {
+	res.send('Api running');
+});
 app.use('/', router);
+
 app.listen(process.env.PORT || 4000, () => {
 	console.log(`app listening at http://localhost`);
 });
-
-
-
-
-
-
-
-
